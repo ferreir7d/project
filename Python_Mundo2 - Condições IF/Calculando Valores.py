05e7cd5a6191ffc4e3b1895f6bfcae8fd6aa8576
daniel <dani.tolaine@hotmail.com>
2024-06-10 14:31:21 -0300

    Calculando valores

---
+++ Python_Mundo2 - Condições IF/Calculando Valores.py
@@ -0,0 +1,14 @@
+for d in range(6, 0, -1):
+    print(d)
+print("fim")
+
+for d in range(0, 3):
+    a = int(input("Digite um número: "))
+print("FIM")
+
+
+s = 0
+for c in range(0, 4):
+    n = int(input("DIGITE UM VALOR: "))
+    s += n
+print("A soma desse todos os números foi de {}".format(s))
