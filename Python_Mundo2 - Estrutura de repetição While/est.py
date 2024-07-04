752cb079126ff642e5421a2c2dd6dbbeaee70fd4
daniel <dani.tolaine@hotmail.com>
2024-07-04 11:43:46 -0300

    test

---
+++ Python_Mundo2 - Estrutura de repetição While/est.py
@@ -0,0 +1,20 @@
+# numero = soma = 0
+# while True:
+#     numero = int(input("Digite um número: "))
+#     if numero == 999:
+#         break
+#     soma = soma + numero
+# print("A soma vale {}".format(soma))
+# print(f"A soma vale {soma}") #interpolação de string, jeito melhor e mais fácil de fazer.
+
+
+
+
+numero = soma = 0
+while True:
+    numero = int(input("Digite um número: "))
+    if numero == 999:
+        break
+    soma += numero
+print(f'A soma vale {soma}')
+
