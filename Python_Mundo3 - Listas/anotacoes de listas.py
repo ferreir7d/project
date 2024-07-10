d05ade669808d3e7bb0e8c30c6f0c9af2ae840dd
daniel <dani.tolaine@hotmail.com>
2024-07-10 16:57:28 -0300

    anots listas

---
+++ Python_Mundo3 - Listas/anotacoes de listas.py
@@ -0,0 +1,9 @@
+numeros = [1, 2, 3]
+numeros[0] = 5
+numeros.append(87) #adiciona elementos.
+numeros.sort() #coloca em ordem.
+numeros.sort(reverse=True) #coloca em ordem do ultimo para o primeiro
+numeros.insert(2, 0) #ele adiciona elementos após indicar no index a posição.
+
+# numeros.pop()
+print(numeros)
