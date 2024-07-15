897e7a25f81bc79c79e7655a8267ea5a11942d63
Daniel Ferreira <dani.tolaine@hotmail.com>
2024-07-15 16:49:20 -0300

    Maior e menor das listas

---
+++ Python_Mundo3 - Listas/Maior e Menor valores na Lista.py
@@ -0,0 +1,10 @@
+valores = []
+for v in range(0,5):
+
+    valores.append(int(input('Digite um valor: ')))
+
+print(f'Os valores digitados foram: {valores}')
+print(f'O maior valor digitado foi {max(valores)}')
+print(f'O menor valor digitado foi {min(valores)}')
+
+
