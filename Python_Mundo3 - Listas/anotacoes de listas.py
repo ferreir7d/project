7c2242431eafaf28b725a75d81028610eee5bcfe
Daniel Ferreira <dani.tolaine@hotmail.com>
2024-07-15 16:49:02 -0300

    anots listas

--- Python_Mundo3 - Listas/anotacoes de listas.py
+++ Python_Mundo3 - Listas/anotacoes de listas.py
@@ -1,9 +1,41 @@
-numeros = [1, 2, 3]
-numeros[0] = 5
-numeros.append(87) #adiciona elementos.
-numeros.sort() #coloca em ordem.
-numeros.sort(reverse=True) #coloca em ordem do ultimo para o primeiro
-numeros.insert(2, 0) #ele adiciona elementos após indicar no index a posição.
+# numeros = [1, 2, 3]
+# numeros[0] = 5
+# numeros.append(87) #adiciona elementos.
+# numeros.sort() #coloca em ordem.
+# numeros.sort(reverse=True) #coloca em ordem do ultimo para o primeiro
+# numeros.insert(2, 0) #ele adiciona elementos após indicar no index a posição.
+#
+# # numeros.pop()
+# print(numeros)
 
-# numeros.pop()
-print(numeros)
+
+# nomes = ['Daniel', 'Aline']
+# nomes.append('Reis')
+# nomes.sort()
+# nomes.insert(0, 'Daniel2')
+# nomes.remove('Daniel2')
+# print(nomes)
+
+
+# nomes = []
+# nomes.append('Daniel')
+# nomes.append('Aline')
+# for n in nomes:
+#     print(f'{n} lindo.')
+
+# nomes = []
+# nomes.append('Daniel')
+# nomes.append('Aline')
+#
+# for letra, nome in enumerate(nomes):
+#     print(f'Na posição {letra} encontrei {nome}')
+# print('Fim')
+
+nomes = []
+for cont in range(0, 5):
+    nomes.append(int(input('Digite um valor: ')))
+
+# a = [11,2,3,4]
+# b = a
+# print(f'Lista A: {a}')
+# print(f'Lista B: {b}')
