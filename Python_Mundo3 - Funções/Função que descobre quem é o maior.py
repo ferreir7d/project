e09878e1b9fe7a6d4ae26d9bd67230d2fbf7d6ad
daniel <dani.tolaine@hotmail.com>
2024-08-12 11:05:01 -0300

    Função que descobre quem é o maior.py

---
+++ Python_Mundo3 - Funções/Função que descobre quem é o maior.py
@@ -0,0 +1,11 @@
+def somar(a=0, b=0, c=0):
+    soma = a + b + c
+    return soma #o return vai ir para a variavel somar1, somar2 e somar3
+
+
+somar1 = somar(3, 2, 5)
+somar2 = somar(1, 1, 1)
+somar3 = somar(2, 2, 2)
+
+print(f'Meus calculos deram {somar1}, {somar2}, {somar3}')
+
