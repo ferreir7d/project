0ad0503d32c084bbf893d19e6a786ab01e7cb21f
daniel <dani.tolaine@hotmail.com>
2024-08-12 11:05:45 -0300

    print com o tamanho exato de caracteres

---
+++ Python_Mundo3 - Funções/Um print especial.py
@@ -0,0 +1,10 @@
+def escreva(mensagem):
+    tamanho = len(mensagem)
+    print('=' * tamanho)
+    print(mensagem)
+    print('=' * tamanho)
+
+
+#programa principal
+escreva('Olá, sou o Daniel!')
+escreva('How are you')
